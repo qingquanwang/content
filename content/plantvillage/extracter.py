5c7db465110b6fb4457bb61cc2c638be97e12b5a
qingquan <qingquan@xiaogu-tech.com>
2017-03-02 16:01:03 +0800

    add question and topic summary

--- content/plantvillage/extracter.py
+++ content/plantvillage/extracter.py
@@ -37,18 +37,58 @@ def extract_topic_page(file):
                 span_tmp.extract()
             dobj['title'] = content_norm(title_sec.get_text())
             disease_secs = d.select('p')
-            dobj['symptoms'] = content_norm(disease_secs[0].get_text())
-            dobj['comments'] = content_norm(disease_secs[1].get_text())
-            dobj['management'] = content_norm(disease_secs[2].get_text())
+            disease_sec_num = len(disease_secs)
+            if disease_sec_num > 0:
+                dobj['symptoms'] = content_norm(disease_secs[0].get_text())
+            if disease_sec_num > 1:
+                dobj['comments'] = content_norm(disease_secs[1].get_text())
+            if disease_sec_num > 2:
+                dobj['management'] = content_norm(disease_secs[2].get_text())
             res['diseases'].append(dobj)
     return res
 def extract_question_page(file):
-    return
+    res = None
+    with open(file, 'r') as fd:
+        res = []
+        p = json.load(fd)
+        for post in p['posts']:
+            post_obj = {}
+            post_obj['title'] = content_norm(post['title'])
+            post_obj['text'] = content_norm(post['text'])
+            if len(post['post_images']) > 0:
+                post_obj['images'] = []
+                for post_image in post['post_images']:
+                    post_obj['images'].append(post_image['url'])
+            res.append(post_obj)
+    return res
 
 
 def main():
-    file = sys.argv[1]
-    print json.dumps(extract_topic_page(file), indent=2).encode('utf-8')
-    #pp.pprint(tmp)
+    directory = sys.argv[1]
+    topics_summary = "plant-village-topics-summary"
+    topics = {}
+    questions_summary = "plant-village-questions-summary"
+    questions = {}
+    if len(sys.argv) > 2:
+        topic_summary = sys.argv[2]
+    if len(sys.argv) > 3:
+        question_summary = sys.argv[3]
+    for subdir in os.listdir(directory):
+        topic= os.path.split(subdir)[1]
+        topic_dir = os.path.join(directory, subdir)
+        topic_page = os.path.join(topic_dir, "diseases_and_pests_description_uses_propagation")
+        question_page = os.path.join(topic_dir, "questions")
+        if os.path.exists(topic_page):
+            topics[topic] = extract_topic_page(topic_page)
+        if os.path.exists(question_page):
+            pp.pprint(question_page)
+            questions[topic] = extract_question_page(question_page)
+            pp.pprint(questions[topic])
+
+    with open(topics_summary, 'w') as topics_summary_fd:
+        topics_summary_fd.write(json.dumps(topics, indent=2).encode('utf-8'))
+    
+    with open(questions_summary, 'w') as questions_summary_fd:
+        questions_summary_fd.write(json.dumps(questions, indent=2).encode('utf-8'))
 if __name__ == '__main__':
     main()
--- tools/plantvillage/crawl-topics.sh
+++ tools/plantvillage/crawl-topics.sh
@@ -6,3 +6,4 @@ wget -k -np -c https://www.plantvillage.org/en/topics
 cat topics | grep topic-square-link | grep -v topic-square-description | sed -e 's/.*href="//g' | sed -e 's/".*$//g' | sort | uniq | xargs -n 1 -P 10 wget -k -np -c -p -L
 cat topics | grep topic-square-questions | sed -e 's/.*href="//g' | sed -e 's/".*$//g' | sort | uniq | xargs -n 1 -P 10 wget -k -np -c -p -L
 rm topics
+python ../../content/plantvillage/extracter.py ./www.plantvillage.org/en/topics/
